5e5da5294c012d0f0ba5f219ba4f6418dba3c1ab
Alessio <16mmstudios@gmail.com>
2022-03-22 22:51:37 +0100

    RSP.js

--- RSP.js
+++ RSP.js
@@ -112,11 +112,9 @@ function capitalize(){
     gameBox.appendChild(pSelct)
     playerSco.setAttribute('class','modPlayer')
    
-
-
     playerSco.textContent =`Player Score:  ${playerScore}`
     gameBox.appendChild(playerSco)
-    
+    playerSco.setAttribute('class','modPlayer')
 
     pcSco.textContent =`Computer Score:  ${computerScore}`
     gameBox.appendChild (pcSco)
